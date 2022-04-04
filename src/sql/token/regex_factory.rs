d9be83d8a2f83d703e94655c942dd23dfd0b402d
xxf098 <xxf098@protonmail.com>
2022-04-04 10:18:24 -0400

    sort_by_length_desc

---
+++ src/sql/token/regex_factory.rs
@@ -0,0 +1,27 @@
+use std::cmp::Ordering;
+use regex::Regex;
+
+
+pub fn sort_by_length_desc(strs: &mut Vec<&str>) {
+    strs.sort_by(|a, b| {
+        let o = b.len().cmp(&a.len());
+        if o == Ordering::Equal { a.cmp(b) } else { o }
+    });
+} 
+
+pub fn create_operator_regex(operators: Vec<&str>) -> Regex {
+    Regex::new(r"").unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sort_by_length_desc() {
+        let mut strs = vec!["aaa", "bb", "ccccc", "ddd", "eeeeeeee", "ff", "h", "g"];
+        sort_by_length_desc(&mut strs);
+        assert_eq!(strs, vec!["eeeeeeee", "ccccc", "aaa", "ddd", "bb", "ff", "g", "h"])
+    }
+    
+}
