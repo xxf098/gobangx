a63a5e2a87a2d43101116279ed7a30b9213780eb
xxf098 <xxf098@protonmail.com>
2022-04-05 03:37:50 -0400

    create_line_comment_regex

--- src/sql/token/regex_factory.rs
+++ src/sql/token/regex_factory.rs
@@ -10,13 +10,20 @@ pub fn sort_by_length_desc(strs: &mut Vec<&str>) {
 }
 
 #[inline]
-pub fn escape_reg_exp<'t>(text: &'t str) -> String {
+pub fn escape_reg_exp(text: &str) -> String {
     regex::escape(text)
 }
 
 pub fn create_operator_regex(mut operators: Vec<&str>) -> anyhow::Result<Regex> {
     sort_by_length_desc(&mut operators);
-    let s = format!(r"^({}|.)", operators.join("|"));
+    let s = format!(r"^({}|.)", operators.iter().map(|s| escape_reg_exp(&s)).collect::<Vec<_>>().join("|"));
+    let reg = Regex::new(&s)?;
+    Ok(reg)
+}
+
+pub fn create_line_comment_regex(line_comment_types: Vec<&str>) -> anyhow::Result<Regex> {
+    let l = line_comment_types.iter().map(|s| escape_reg_exp(&s)).collect::<Vec<_>>().join("|");
+    let s = format!(r"^((?:{}).*?)(?:\r\n|\r|\n|$)", l); 
     let reg = Regex::new(&s)?;
     Ok(reg)
 }
@@ -36,6 +43,13 @@ mod tests {
     fn test_escape_reg_exp() {
         let s = escape_reg_exp("?");
         assert_eq!(s, r"\?");
+    }
+
+    #[test]
+    fn test_create_line_comment_regex() {
+        let line_comment_types = vec!["--"];
+        let reg = create_line_comment_regex(line_comment_types).unwrap();
+        assert_eq!(reg.as_str(), r"^((?:\-\-).*?)(?:\r\n|\r|\n|$)");
     }
 
     #[test]
--- src/sql/token/tokenizer.rs
+++ src/sql/token/tokenizer.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 use super::token_type::TokenType;
-use super::regex_factory::create_operator_regex;
+use super::regex_factory::{create_operator_regex, create_line_comment_regex};
 
 pub struct TokenizerConfig<'a> {
     pub reserved_words: Vec<&'a str>,
@@ -32,6 +32,7 @@ pub struct Tokenizer {
     number_regex: Regex,
     operator_regex: Regex,
     block_comment_regex: Regex,
+    line_comment_regex: Regex,
 }
 
 impl Tokenizer {
@@ -40,7 +41,8 @@ impl Tokenizer {
             whitespace_regex: Regex::new(r"^(\s+)")?, 
             number_regex: Regex::new(r"^((-\s*)?[0-9]+(\.[0-9]+)?([eE]-?[0-9]+(\.[0-9]+)?)?|0x[0-9a-fA-F]+|0b[01]+)\b")?,
             operator_regex: create_operator_regex(vec!["<>", "<=", ">="])?,
-            block_comment_regex: Regex::new(r"^(/\*[^]*?(?:\*/|$))")?,
+            block_comment_regex: Regex::new(r"^(/\*[\S\s]*?(?:\*/|$))")?,
+            line_comment_regex: create_line_comment_regex(cfg.line_comment_types)?,
         };
         Ok(t)
     }
