0b6ca6b7246e9aea9194ae60387b25564fc95430
xxf098 <xxf098@protonmail.com>
2022-04-15 03:47:02 -0400

    rename to TokenType

--- src/sql/parse/keywords.rs
+++ src/sql/parse/keywords.rs
@@ -1,22 +1,22 @@
-use regex::Regex;
-use super::Token;
+use regex::{Regex, RegexBuilder};
+use super::TokenType;
 use crate::sql::token::regex_factory::{ create_string_regex };
 
 pub struct RegexToken {
-    reg: Regex,
-    token: Token,
+    pub reg: Regex,
+    token: TokenType,
 }
 
 impl RegexToken {
     
-    fn new(s: &str, token: Token) -> Self {
+    fn new(s: &str, token: TokenType) -> Self {
         Self{
-            reg: Regex::new(s).unwrap(), 
+            reg: RegexBuilder::new(s).case_insensitive(true).build().unwrap(), 
             token
         }
     }
 
-    fn new_reg(r: Regex, token: Token) -> Self {
+    fn new_reg(r: Regex, token: TokenType) -> Self {
         Self{
             reg: r, 
             token
@@ -25,73 +25,74 @@ impl RegexToken {
 }
 
 #[inline]
-fn new_rt(s: &str, token: Token) -> RegexToken{
+fn new_rt(s: &str, token: TokenType) -> RegexToken{
     RegexToken::new(s, token)
 }
 
 
-fn sql_regex() -> Vec<RegexToken> {
+pub fn sql_regex() -> Vec<RegexToken> {
     let literal = create_string_regex(vec!["''", r#""""#, "``"]).unwrap();
+
     vec![
-        new_rt(r"(--|# )\+.*?(\r\n|\r|\n|$)", Token::CommentSingleHint),
-        new_rt(r"/\*\+[\s\S]*?\*/", Token::CommentMultilineHint),
+        new_rt(r"(--|# )\+.*?(\r\n|\r|\n|$)", TokenType::CommentSingleHint),
+        new_rt(r"/\*\+[\s\S]*?\*/", TokenType::CommentMultilineHint),
 
-        new_rt(r"(--|# ).*?(\r\n|\r|\n|$)", Token::CommentSingle),
-        new_rt(r"/\*[\s\S]*?\*/", Token::CommentMultiline),
+        new_rt(r"(--|# ).*?(\r\n|\r|\n|$)", TokenType::CommentSingle),
+        new_rt(r"/\*[\s\S]*?\*/", TokenType::CommentMultiline),
 
-        new_rt(r"(\r\n|\r|\n)", Token::Newline),
-        new_rt(r"\s+?", Token::Whitespace),
+        new_rt(r"(\r\n|\r|\n)", TokenType::Newline),
+        new_rt(r"\s+?", TokenType::Whitespace),
 
-        new_rt(r":=", Token::Assignment),
-        new_rt(r"::", Token::Punctuation),
+        new_rt(r":=", TokenType::Assignment),
+        new_rt(r"::", TokenType::Punctuation),
 
-        new_rt(r"\*", Token::Wildcard),
+        new_rt(r"\*", TokenType::Wildcard),
 
-        new_rt(r"`(``|[^`])*`", Token::Name),
-        new_rt(r"´(´´|[^´])*´", Token::Name),
-        RegexToken::new_reg(literal, Token::Literal),
+        new_rt(r"`(``|[^`])*`", TokenType::Name),
+        new_rt(r"´(´´|[^´])*´", TokenType::Name),
+        RegexToken::new_reg(literal, TokenType::Literal),
 
-        new_rt(r"\?", Token::NamePlaceholder),
-        new_rt(r"%(\(\w+\))?s", Token::NamePlaceholder),
+        new_rt(r"\?", TokenType::NamePlaceholder),
+        new_rt(r"%(\(\w+\))?s", TokenType::NamePlaceholder),
         // (r'(?<!\w)[$:?]\w+', tokens.Name.Placeholder),
 
-        new_rt(r"\\\w+", Token::Command),
-        new_rt(r"(NOT\s+)?(IN)\b", Token::OperatorComparison),
+        new_rt(r"\\\w+", TokenType::Command),
+        new_rt(r"(NOT\s+)?(IN)\b", TokenType::OperatorComparison),
 
-        new_rt(r"(CASE|IN|VALUES|USING|FROM|AS)\b", Token::Keyword),
+        new_rt(r"(CASE|IN|VALUES|USING|FROM|AS)\b", TokenType::Keyword),
 
-        new_rt(r"(@|##|#)[A-ZÀ-Ü]\w+", Token::Name),
-        new_rt(r"[A-ZÀ-Ü]\w*(?:\s*\.)", Token::Name),
-        new_rt(r"(\.)[A-ZÀ-Ü]\w*", Token::Name),
-        new_rt(r"[A-ZÀ-Ü]\w*(?:\()", Token::Name),
+        new_rt(r"(@|##|#)[A-ZÀ-Ü]\w+", TokenType::Name),
+        new_rt(r"[A-ZÀ-Ü]\w*(?:\s*\.)", TokenType::Name),
+        new_rt(r"(\.)[A-ZÀ-Ü]\w*", TokenType::Name),
+        new_rt(r"[A-ZÀ-Ü]\w*(?:\()", TokenType::Name),
 
-        new_rt(r"-?0x[\dA-F]+", Token::NumberHexadecimal),
-        new_rt(r"-?\d+(\.\d+)?E-?\d+", Token::NumberFloat),
-        new_rt(r"-?(\d+(\.\d*)|\.\d+)", Token::NumberFloat),
-        new_rt(r"(-\s*)?[0-9]+", Token::NumberInteger),
+        new_rt(r"-?0x[\dA-F]+", TokenType::NumberHexadecimal),
+        new_rt(r"-?\d+(\.\d+)?E-?\d+", TokenType::NumberFloat),
+        new_rt(r"-?(\d+(\.\d*)|\.\d+)", TokenType::NumberFloat),
+        new_rt(r"(-\s*)?[0-9]+", TokenType::NumberInteger),
 
-        new_rt(r"'(''|\\\\|\\'|[^'])*'", Token::StringSingle),
-        new_rt(r#""(""|\\\\|\\"|[^"])*""#, Token::StringSymbol),
-        new_rt(r#"(""|".*?[^\\]")"#, Token::StringSymbol),
-        new_rt(r#"(?:[^\w\])])(\[[^\]\[]+\])"#, Token::Name),
+        new_rt(r"'(''|\\\\|\\'|[^'])*'", TokenType::StringSingle),
+        new_rt(r#""(""|\\\\|\\"|[^"])*""#, TokenType::StringSymbol),
+        new_rt(r#"(""|".*?[^\\]")"#, TokenType::StringSymbol),
+        new_rt(r#"(?:[^\w\])])(\[[^\]\[]+\])"#, TokenType::Name),
 
-        new_rt(r"((LEFT\s+|RIGHT\s+|FULL\s+)?(INNER\s+|OUTER\s+|STRAIGHT\s+)?|(CROSS\s+|NATURAL\s+)?)?JOIN\b", Token::Keyword),
-        new_rt(r"END(\s+IF|\s+LOOP|\s+WHILE)?\b", Token::Keyword),
-        new_rt(r"NOT\s+NULL\b", Token::Keyword),
-        new_rt(r"NULLS\s+(FIRST|LAST)\b", Token::Keyword),
-        new_rt(r"UNION\s+ALL\b", Token::Keyword),
-        new_rt(r"CREATE(\s+OR\s+REPLACE)?\b", Token::KeywordDDL),
-        new_rt(r"DOUBLE\s+PRECISION\b", Token::NameBuiltin),
-        new_rt(r"GROUP\s+BY\b", Token::Keyword),
-        new_rt(r"ORDER\s+BY\b", Token::Keyword),
-        new_rt(r"HANDLER\s+FOR\b", Token::Keyword),
-        new_rt(r"(LATERAL\s+VIEW\s+)(EXPLODE|INLINE|PARSE_URL_TUPLE|POSEXPLODE|STACK)\b", Token::Keyword),
-        new_rt(r"(AT|WITH')\s+TIME\s+ZONE\s+'[^']+'", Token::KeywordTZCast),
-        new_rt(r"(NOT\s+)?(LIKE|ILIKE|RLIKE)\b", Token::OperatorComparison),
-        // (r'[0-9_A-ZÀ-Ü][_$#\w]*', is_keyword),
-        new_rt(r"[;:()\[\],\.]", Token::Punctuation),
-        new_rt(r"[<>=~!]+", Token::OperatorComparison),
-        new_rt(r"[+/@#%^&|^-]+", Token::Operator)
+        new_rt(r"((LEFT\s+|RIGHT\s+|FULL\s+)?(INNER\s+|OUTER\s+|STRAIGHT\s+)?|(CROSS\s+|NATURAL\s+)?)?JOIN\b", TokenType::Keyword),
+        new_rt(r"END(\s+IF|\s+LOOP|\s+WHILE)?\b", TokenType::Keyword),
+        new_rt(r"NOT\s+NULL\b", TokenType::Keyword),
+        new_rt(r"NULLS\s+(FIRST|LAST)\b", TokenType::Keyword),
+        new_rt(r"UNION\s+ALL\b", TokenType::Keyword),
+        new_rt(r"CREATE(\s+OR\s+REPLACE)?\b", TokenType::KeywordDDL),
+        new_rt(r"DOUBLE\s+PRECISION\b", TokenType::NameBuiltin),
+        new_rt(r"GROUP\s+BY\b", TokenType::Keyword),
+        new_rt(r"ORDER\s+BY\b", TokenType::Keyword),
+        new_rt(r"HANDLER\s+FOR\b", TokenType::Keyword),
+        new_rt(r"(LATERAL\s+VIEW\s+)(EXPLODE|INLINE|PARSE_URL_TUPLE|POSEXPLODE|STACK)\b", TokenType::Keyword),
+        new_rt(r"(AT|WITH')\s+TIME\s+ZONE\s+'[^']+'", TokenType::KeywordTZCast),
+        new_rt(r"(NOT\s+)?(LIKE|ILIKE|RLIKE)\b", TokenType::OperatorComparison),
+        new_rt(r"[0-9_A-ZÀ-Ü][_$#\w]*", TokenType::KeywordRaw),
+        new_rt(r"[;:()\[\],\.]", TokenType::Punctuation),
+        new_rt(r"[<>=~!]+", TokenType::OperatorComparison),
+        new_rt(r"[+/@#%^&|^-]+", TokenType::Operator)
     ]
 }
 
