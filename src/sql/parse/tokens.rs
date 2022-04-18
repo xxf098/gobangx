d66714bee65f1ae79eb8e737a2334b9dd5ac1ca4
xxf098 <xxf098@protonmail.com>
2022-04-18 02:06:57 -0400

    TokenType

---
+++ src/sql/parse/tokens.rs
@@ -0,0 +1,47 @@
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum TokenType {
+    Text,
+    Whitespace,
+    Newline,
+    Error,
+    Other,
+    // Common token types for source code
+    Keyword,
+    KeywordRaw, // need check manually
+    KeywordDDL,
+    KeywordDML,   
+    KeywordCTE,
+    KeywordTZCast, 
+    Name,
+    NamePlaceholder,
+    NameBuiltin,
+
+    Literal,
+    String,
+    StringSingle,
+    StringSymbol,
+    Number,
+    NumberHexadecimal,
+    NumberFloat,
+    NumberInteger,    
+    Punctuation,
+    Operator,
+    OperatorComparison,
+    Comparison,
+    Wildcard,
+    Comment,
+    CommentSingle,
+    CommentSingleHint,
+    CommentMultiline,
+    CommentMultilineHint,
+    Assignment,
+    // Generic types for non-source code
+    Generic,
+    Command,
+    // String and some others are not direct children of Token.
+    Token,
+    DML,
+    DDL,
+    CTE,
+}
