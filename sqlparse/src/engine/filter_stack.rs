1ed56758d36bdd28376cc792b67b4d1a0059a928
xxf098 <xxf098@protonmail.com>
2022-05-17 02:34:45 -0400

    format sql

--- sqlparse/src/engine/filter_stack.rs
+++ sqlparse/src/engine/filter_stack.rs
@@ -6,13 +6,20 @@ use crate::filters::Filter;
 pub struct FilterStack {
     regs: Vec<RegexToken>,
     preprocess: Vec<Box<dyn Filter>>,
+    stmtprocess: Vec<Box<dyn Filter>>,
+    postprocess: Vec<Box<dyn Filter>>,
 }
 
 
 impl FilterStack {
 
     pub fn new() -> Self {
-        Self { regs: sql_regex(), preprocess: vec![] }
+        Self { 
+            regs: sql_regex(), 
+            preprocess: vec![],
+            stmtprocess: vec![],
+            postprocess: vec![], 
+        }
     }
 
     // TODO: support more than one sql
@@ -20,6 +27,17 @@ impl FilterStack {
         let mut tokens = tokenize_internal(sql, &self.regs);
         if grouping {
             tokens = super::grouping::group(tokens);
+        }
+        tokens
+    }
+
+    // format sql
+    pub fn format(&self, sql: &str) -> Vec<Token> {
+        let mut tokens = tokenize_internal(sql, &self.regs);
+        for token in tokens.iter_mut() {
+            self.preprocess.iter().for_each(|filter| filter.process(token));
+            self.stmtprocess.iter().for_each(|filter| filter.process(token));
+            self.postprocess.iter().for_each(|filter| filter.process(token));
         }
         tokens
     }
