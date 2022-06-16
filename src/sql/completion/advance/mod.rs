bf68b3033bbc1cafc2f53e7b200d6f2cd5838fea
xxf098 <xxf098@protonmail.com>
2022-06-15 23:33:26 -0400

    test_select_suggests_cols_with_qualified_table_scope

--- src/sql/completion/advance/mod.rs
+++ src/sql/completion/advance/mod.rs
@@ -25,12 +25,25 @@ mod tests {
     }
 
     #[test]
+    fn test_select_suggests_cols_with_qualified_table_scope() {
+        let suggest = Suggest::default();
+        let sql = "SELECT  FROM sch.tabl";
+        let types = suggest.suggest_type(sql, "SELECT ");
+        assert_eq!(types.len(), 4);
+        assert_eq!(types[0], SuggestType::column(Some("sch"), "tabl", None));
+        assert_eq!(types[1], SuggestType::Function("".to_string()));
+        assert_eq!(types[2], SuggestType::Alias(vec!["tabl".to_string()]));
+        assert_eq!(types[3], SuggestType::Keyword);
+    }
+
+    #[test]
     fn test_suggest_where_suggests_columns_functions() {
         let suggest = Suggest::default();
         let sqls = vec![
             "SELECT * FROM tabl WHERE ",
             // "SELECT * FROM tabl WHERE ("
         ];
+        // FIXME: 
         for sql in sqls {
             let types = suggest.suggest_type(sql, sql);
             // println!("{:?}", types);
