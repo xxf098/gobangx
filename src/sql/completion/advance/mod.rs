1d2a7d97a9fd4e33757230460de010b6714d8250
xxf098 <xxf098@protonmail.com>
2022-06-17 02:32:34 -0400

    test_where_equals_any_suggests_columns_or_keywords

--- src/sql/completion/advance/mod.rs
+++ src/sql/completion/advance/mod.rs
@@ -12,6 +12,11 @@ use completion_engine::{SuggestTable, SuggestType, Suggest};
 mod tests {
     use super::*;
 
+    fn suggest_type(full_text: &str, text_before_cursor: &str) -> Vec<SuggestType>  {
+        let suggest = Suggest::default();
+        suggest.suggest_type(full_text, text_before_cursor)
+    }
+
     #[test]
     fn test_select_suggests_cols_with_visible_table_scope() {
         let suggest = Suggest::default();
@@ -64,14 +69,45 @@ mod tests {
     #[test]
     fn test_where_in_suggests_columns() {
         let suggest = Suggest::default();
-        let sql = "SELECT * FROM tabl WHERE foo IN (";
-        let now = std::time::Instant::now();
+        let sqls = vec![
+            "SELECT * FROM tabl WHERE foo IN (",
+            "SELECT * FROM tabl WHERE foo IN (bar, ",
+            ];
+        for sql in sqls {
+            let now = std::time::Instant::now();
+            let types = suggest.suggest_type(sql, sql);
+            let elapsed = now.elapsed();
+            println!("elapsed: {}ms", elapsed.as_millis());
+            assert_eq!(types[0], SuggestType::column(None, "tabl", None));
+            assert_eq!(types[1], SuggestType::Function("".to_string()));
+            assert_eq!(types[2], SuggestType::Alias(vec!["tabl".to_string()]));
+            assert_eq!(types[3], SuggestType::Keyword);
+        }
+    }
+
+    #[test]
+    fn test_where_equals_any_suggests_columns_or_keywords() {
+        let suggest = Suggest::default();
+        let sql = "SELECT * FROM tabl WHERE foo = ANY(";
         let types = suggest.suggest_type(sql, sql);
-        let elapsed = now.elapsed();
-        println!("elapsed: {}ms", elapsed.as_millis());
         assert_eq!(types[0], SuggestType::column(None, "tabl", None));
         assert_eq!(types[1], SuggestType::Function("".to_string()));
         assert_eq!(types[2], SuggestType::Alias(vec!["tabl".to_string()]));
         assert_eq!(types[3], SuggestType::Keyword);
     }
+
+
+    #[test]
+    fn test_lparen_suggests_cols() {
+        let sql = "SELECT MAX( FROM tbl";
+        let types = suggest_type(sql, "SELECT MAX(");
+        assert_eq!(types[0], SuggestType::column(None, "tbl", None));
+    }
+
+    #[test]
+    fn test_operand_inside_function_suggests_cols1() {
+        let types = suggest_type("SELECT MAX(col1 +  FROM tbl", "SELECT MAX(col1 + ");
+        println!("{:?}", types);
+    }
+
 }
